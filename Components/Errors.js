e37942404f4c2f7a8ba451397f699e1f09cf71f5
ErnestoOrtegaHernandez <ernesto.ortega53@gmail.com>
2022-04-21 21:24:41 -0400

    sorting complete

--- Components/Errors.js
+++ Components/Errors.js
@@ -24,7 +24,61 @@ export default function Errors({navigation}) {
 
   }, [])
 
-  console.log(errorDetails);
+  let frequencySort = (sortParam, dateSortedArr) =>{
+    let freq = {};
+    for (let i = 0; i < dateSortedArr.length; i++) {
+      let key = dateSortedArr[i][sortParam];
+      if (!freq[key]) {
+        freq[key] = [dateSortedArr[i]];
+      } else {
+        freq[key].push(dateSortedArr[i]);
+      }
+    }
+
+    let sortedbyFreq = [];
+
+    let longest;
+    while(Object.keys(freq).length > 0) {
+      for(let key in freq){
+        if(longest === undefined) longest = key
+        else if(freq[key].length > freq[longest].length) longest = key
+        else if(freq[key].length === freq[longest].length) {
+          break;
+        }
+      }
+      if(sortedbyFreq.length === 0) sortedbyFreq = freq[longest];
+      else sortedbyFreq = sortedbyFreq.concat(freq[longest]);
+      delete freq[longest];
+      longest = undefined;
+    }
+
+    return sortedbyFreq;
+  }
+
+  const handleSort = (sortBy , direction) => {
+
+    let sortedByDate = errorDetails.sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    });
+    let sortCopy = [...sortedByDate];
+    if(sortBy === 'date'){
+      if(direction === 'asc') setErrorDetails(sortCopy);
+      else setErrorDetails(sortCopy.reverse());
+
+    } else if (sortBy === 'type'){
+      let sortedByType = frequencySort(sortBy, sortedByDate);
+      if(direction === 'asc') setErrorDetails(sortedByType);
+      else setErrorDetails(sortedByType.reverse());
+    } else if (sortBy === 'robotId'){
+      let sortedById = frequencySort(sortBy, sortedByDate);
+      if(direction === 'asc') setErrorDetails(sortedById);
+      else setErrorDetails(sortedById.reverse());
+    } else if (sortBy === 'task'){
+      let sortedByTask = frequencySort(sortBy, sortedByDate);
+      if(direction === 'asc') setErrorDetails(sortedByTask);
+      else setErrorDetails(sortedByTask.reverse());
+    }
+  }
 
   return (
     <>
@@ -33,46 +87,46 @@ export default function Errors({navigation}) {
         <View style = {{flexDirection:"row"}}>
           <View style = {styles.column}>
             <Text style = {styles.header}>Robot ID</Text>
-            <TouchableHighlight>
+            <TouchableHighlight onPress = {()=> handleSort('robotId', 'asc')} >
               <TriangleSymbol up = {true}/>
             </TouchableHighlight>
-            <TouchableHighlight>
+            <TouchableHighlight onPress = {()=> handleSort('robotId', 'desc')}>
               <TriangleSymbol up = {false}/>
             </TouchableHighlight>
           </View>
           <View style = {styles.column}>
             <Text style = {styles.header}>Error Type</Text>
-            <TouchableHighlight>
+            <TouchableHighlight onPress = {()=> handleSort('type', 'asc')}>
               <TriangleSymbol up = {true}/>
             </TouchableHighlight>
-            <TouchableHighlight>
+            <TouchableHighlight onPress = {()=> handleSort('type', 'desc')}>
               <TriangleSymbol up = {false}/>
             </TouchableHighlight>
           </View>
           <View style = {styles.column}>
             <Text style = {styles.header}>Date/Time</Text>
-            <TouchableHighlight>
+            <TouchableHighlight onPress = {()=> handleSort('date', 'asc')}>
               <TriangleSymbol up = {true}/>
             </TouchableHighlight>
-            <TouchableHighlight>
+            <TouchableHighlight onPress = {()=> handleSort('date', 'desc')}>
               <TriangleSymbol up = {false}/>
             </TouchableHighlight>
           </View>
           <View style = {styles.column}>
             <Text style = {styles.header}>Task</Text>
-            <TouchableHighlight>
+            <TouchableHighlight onPress = {()=> handleSort('task', 'asc')}>
               <TriangleSymbol up = {true}/>
             </TouchableHighlight>
-            <TouchableHighlight>
+            <TouchableHighlight onPress = {()=> handleSort('task', 'desc')}>
               <TriangleSymbol up = {false}/>
             </TouchableHighlight>
           </View>
         </View>
         <ScrollView>
-          {errorDetails.length > 0 ? errorDetails.map((robot, index) => {
-            return robot.errors.map((error, index) => {
-              return <ErrorEntry key = {index} id = {robot.robotId} type = {error.type} date = {error.date} task = {error.task} />
-            })
+          {errorDetails.length > 0 ? errorDetails.map((error, index) => {
+
+            return <ErrorEntry key = {index} id = {error.robotId} type = {error.type} date = {error.date} task = {error.task} />
+
           }) : <Text>No errors to report</Text>}
         </ScrollView>
       </View>
