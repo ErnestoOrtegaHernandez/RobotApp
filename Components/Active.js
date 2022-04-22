c03ccd18c1f44a6c5f274176bdeff452e8d2bac9
ErnestoOrtegaHernandez <ernesto.ortega53@gmail.com>
2022-04-22 13:08:15 -0400

    Static Active Screen complete

--- Components/Active.js
+++ Components/Active.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState}from 'react';
-import { StyleSheet, Text, View, Button, TextInput, NavigationContainer } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, NavigationContainer, Image } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Header from './Header';
 import Footer from './Footer';
@@ -16,6 +16,26 @@ export default function Active({navigation}) {
     <>
       <Header navigation = {navigation} title = {'Active Robots'}/>
       <View style={styles.active}>
+        <View style = {styles.infoBar}>
+          <View style = {{flexDirection:"row", flex: 1}}>
+            <Text style = {styles.robotNumber}>1 </Text>
+            <View style = {{flexDirection: "column",  alignItems: 'left'}}>
+              <Text style = {styles.infoText}>Task: Unload</Text>
+              <Text style = {styles.infoText}>Completion: 43%</Text>
+            </View>
+          </View>
+          <View style = {{flexDirection:"row", flex: 1}}>
+            <Text style = {styles.robotNumber}>2 </Text>
+            <View style = {{flexDirection: "column", alignItems: 'left'}}>
+              <Text style = {styles.infoText}>Task: Assemble</Text>
+              <Text style = {styles.infoText}>Completion: 53%</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style = {styles.floorPlanContainer}>
+          <Image source={require("./floorplan.png")} style ={styles.image}/>
+        </View>
       </View>
       <Footer/>
     </>
@@ -26,8 +46,36 @@ export default function Active({navigation}) {
 const styles = StyleSheet.create({
   active: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'left',
     justifyContent: 'center',
+    flexDirection: 'row'
   },
+  infoBar: {
+    padding: '1em',
+    height: '100%',
+    flex: 3,
+    backgroundColor: '#000',
+  },
+  floorPlanContainer: {
+    padding: '1em',
+    height: '100%',
+    flex: 6,
+    backgroundColor: '#fff',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    resizeMode: 'contain'
+  },
+  robotNumber: {
+    fontSize: '2em',
+    color: '#fff',
+    fontWeight: 'bold',
+    // justifyContent: 'center',
+    alignItems: 'center'
+  },
+  infoText: {
+    fontSize: '1em',
+    color: '#fff'
+  }
 });
